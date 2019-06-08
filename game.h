3833d7fc69878e884eff1cc1e5e0641c6b9d6113
Matthew Bawalan <matthewbawalan@gmail.com>
2019-06-07 18:58:38 -0700

    confirmed for mac, added comments

--- game.h
+++ game.h
@@ -6,9 +6,12 @@
 #include <ctime>
 
 int roll(){
+	//randomizing the seed
 	srand((int)time(0));
 	std::cout << rand() << std::endl;
 	srand(rand());
+
+	//rolling
 	return (rand() % 6) + 1;
 }
 #endif
